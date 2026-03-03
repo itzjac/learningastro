8b3c3ceb873f0b7c833eff58c432a6fad401ed85
itzjac <isaacvegan@gmail.com>
2026-03-02 18:47:13 -0600

    Optimized images in the slug

--- 03-blog/src/content/config.ts
+++ 03-blog/src/content/config.ts
@@ -2,12 +2,15 @@ import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection( {
     type: 'content',
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         date:  z.date(),
         description:  z.string(),
-        image: z.string(),
-
+        image: image(),
+        // TODO(dagon): refine doesn't work
+        // image: image().refine ( (img) => img.width < 1200, {
+        // image: 'Image should be greated than 1200px' 
+        //}),
         author: z.string(),
         tags: z.array(z.string()),
     })
