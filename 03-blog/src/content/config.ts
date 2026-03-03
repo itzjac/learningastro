8f166fc7e33c21182427d65a3a16219d9d68a6c3
itzjac <isaacvegan@gmail.com>
2026-03-03 16:03:09 -0600

    Cross reference author/blog-posts, collections, first Promise.all

--- 03-blog/src/content/config.ts
+++ 03-blog/src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection( {
     type: 'content',
@@ -11,12 +11,24 @@ const blogCollection = defineCollection( {
         // image: image().refine ( (img) => img.width < 1200, {
         // image: 'Image should be greated than 1200px' 
         //}),
-        author: z.string(),
+        //author: z.string(),
+        author: reference('author'),
         tags: z.array(z.string()),
+
+        isDraft: z.boolean().default(false),
+    })
+});
+
+const authorCollection = defineCollection ( {
+    type: "data",
+    schema: ({ image}) => z.object( {
+        name: z.string(),
+        avatar: image(),
     })
 });
 
 export const collections = {
     blog: blogCollection,
+    author: authorCollection,
 };
 
--- 03-blog/src/utils/formatter.ts
+++ 03-blog/src/utils/formatter.ts
@@ -1,11 +1,26 @@
 export class Formatter {
     static formatDate ( value: Date): string {
+
         const date = new Date(value);
+
+        const day = date.getDate();
+
+        const getDaySuffix = (day: number): string => {
+            if (day >= 11 && day <= 13) return 'th'; // Special case for 11th, 12th, 13th
+            switch (day % 10) {
+                case 1: return 'st';
+                case 2: return 'nd';
+                case 3: return 'rd';
+                default: return 'th';
+            }
+        };
+       
         const options: Intl.DateTimeFormatOptions = {
             month: 'long',
-            day: '2-digit',
             year: 'numeric'
             } as const; // Add 'as const' here
-        return Intl.DateTimeFormat('en-US', options).format(date);
+       
+        const formattedDate = Intl.DateTimeFormat('en-US', options).format(date);
+        return formattedDate.replace(/(\w+ )(\d{4})/, `$1${day}${getDaySuffix(day)}, $2`);
     }
 }
