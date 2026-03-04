7ba474a56191c3f57286278776b9daee5a672e40
itzjac <isaacvegan@gmail.com>
2026-03-04 15:22:33 -0600

    Favorite pokemon card tsx and interface

---
+++ 02-pagination/src/components/pokemons/FavoritePokemons.tsx
@@ -0,0 +1,21 @@
+import type { FavoritePokemon } from "src/interfaces/favorite-pokemon";
+import { For, createSignal } from 'solid-js';
+import { FavoritePokemonCard } from "./FavoritePokemonCard";
+
+const getLocalStoragePokemons = (): FavoritePokemon[] => {
+    const favoritePokemons = JSON.parse(
+        localStorage.getItem('favorites') ?? '[]'
+    );
+    return favoritePokemons;
+}
+
+export const FavoritePokemons = () => {
+
+    const [pokemons, setPokemons] = createSignal(getLocalStoragePokemons());
+
+    return (
+        <div class="grid grid-cols-2 sm:gri-cols-4">
+            <For each={ pokemons() }>{ (pokemon) => <FavoritePokemonCard pokemon={pokemon}/>}</For>
+        </div>
+    );
+}
